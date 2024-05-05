3f4a9aae2d2afa2bfb9553a138bb1b68d694895c
fnasyr <fnasyr000@gmail.com>
2024-05-04 21:26:35 -0400

    Passed props to Comment component

--- social-/src/components/Post.jsx
+++ social-/src/components/Post.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Comment from './Comment';
 function Post(){
 return(
-<div>
+    <div>
 <h3>Test Post</h3>
 <p>This is a test post we'll edit later!</p>
 <p>Likes: 30</p>
 <p>Comments:</p>
-<Comment />
+<Comment content="This is a test comment!" />
+<Comment content="This is another test comment!" />
+<Comment content="This is yet another test comment!" />
 </div>
 )
 }
